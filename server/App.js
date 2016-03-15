61937b369ebaa7a4b00fb7e38e35917568b381e5
Natalie <natalie.s.sharpe@gmail.com>
2016-03-15 01:37:35 -0500

    progress (that looks ugly) on styling of navbar and game page

--- server/App.js
+++ server/App.js
@@ -7,7 +7,7 @@ var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var browserHistory = ReactRouter.browserHistory;
 
-var Stylesheet = require('../public/styles.css');
+// var Stylesheet = require('../public/styles.css');
 var Index = require('../app/components/index/Index');
 var Signin = require('../app/components/signin/Signin');
 var Signup = require('../app/components/signup/Signup');
@@ -34,14 +34,15 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-          <div className='navbar'>
-            <h1>Fwibble</h1>
-            <ul className='nav-links'>
-
-              <li><Link to='/signin' className='fa fa-user'>Sign In</Link></li>
-              <li><Link to='/gameview' className='fa fa-pencil'>Game</Link></li>
+        <div className='navbar navbar-default navbar-static-top'>
+          <div className='container'>
+            <Link to='/' className='navbar-brand'>Fwibble</Link>
+            <ul className='nav navbar-nav navbar-right' className='nav-links' className='list-inline'>
+              <li><button type='button' className='btn btn-lg btn-success'><Link to='/signin'>Sign In</Link></button></li>
+              <li><button type='button' className='btn btn-lg btn-success'><Link to='/gameview'>Game</Link></button></li>
             </ul>
           </div>
+        </div>
           {this.props.children && React.cloneElement(this.props.children, {
             setUser: this.setUser,
             user: this.state.username
