f505f0db74ebcb71d1c166873933bd3690a4d0db
Natalie <natalie.s.sharpe@gmail.com>
2016-03-26 12:30:58 -0500

    reverted to old nav bar

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -9,10 +9,11 @@ module.exports = React.createClass({
   render: function() {
 
     return (
-      <nav className='navbar navbar-default navbar-static-top'>
+      <div className='navbar navbar-default navbar-static-top'>
         <div className='container-fluid'>
           <div className='navbar-header'>
             <Link to='/' className='navbar-brand' >Fwibble</Link>
+          </div>
             <ul className="nav navbar-nav navbar-right">
               <li>
                 { this.props.loggedIn ? (<Link to='/signout'>SIGN OUT</Link>) : (<Link to='/signin'>SIGN IN</Link>)}
@@ -20,32 +21,7 @@ module.exports = React.createClass({
               <li><Link to={`/gameview/${this.props.active_game}`} className="menuOptions">MY GAME</Link></li>
             </ul>
           </div>
-
-          <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-              <li>
-                { this.props.loggedIn ? (<Link to='/signout'>SIGN OUT</Link>) : (<Link to='/signin'>SIGN IN</Link>)}
-              </li>
-              <li><Link to={`/gameview/${this.props.active_game}`} className="menuOptions">MY GAME</Link></li>
-            </ul>
-          </div>
         </div>
-      </nav>
     )
   }
 });
-
-
-
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li> 
-        <li><a href="#">Page 3</a></li> 
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
